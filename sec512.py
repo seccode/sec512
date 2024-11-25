af2e35e012e87819c8d2619b3122a47f8068a1be
seccode <42663376+seccode@users.noreply.github.com>
2024-11-25 12:00:07 -0800

    Update sec512.py

--- sec512.py
+++ sec512.py
@@ -2,7 +2,7 @@ import random
 
 def hash(s):
     random.seed(s)
-    return "".join(random.choices("0123456789abcdef",k=64))
+    return "".join(random.choices("0123456789abcdef",k=128))
 
 if __name__=="__main__":
     s="This is a test string to hash"
